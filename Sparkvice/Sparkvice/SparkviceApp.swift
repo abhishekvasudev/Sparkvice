3206ce28e64ace4a7d05e4bd3562a0f3564ddddf
Abhishek Vasudev <abhishekvasudev7@gmail.com>
2025-05-24 22:13:00 +0530

    Initial project

---
+++ Sparkvice/Sparkvice/SparkviceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparkviceApp.swift
+//  Sparkvice
+//
+//  Created by Abhishek Vasudev on 24/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SparkviceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
